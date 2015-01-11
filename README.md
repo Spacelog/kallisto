9948f2a71f19637c8270cac599b72398395fe2d1
James Aylett <james@tartarus.org>
2015-01-11 00:03:32 +0000

    Speculative & untested fabric for deployment.

--- README.md
+++ README.md
@@ -13,6 +13,17 @@ This is very early days, but:
  * `python manage.py import_pages` will import the text and PNG pages; see its help for how (and how to generate them)
  * run tests with `python manage.py collectstatic --noinput` followed by `python manage.py test` (the test rig doesn't look in source locations for static files, so you have to do the static collection deploy step first: note that if you change settings to eg put static files on S3 then this isn't a good solution)
 
+## Deploying a live instance
+
+Via fabric. Check out `fab -l` for help, then:
+
+```
+$ fab -H $HOST setup:{ SCARY AMOUNTS OF CONFIGURATION }
+$ fab -H $HOST deploy
+```
+
+should do what you want. Won't help you with setting up a database, or directing your website to forward to the gunicorn instance.
+
 ## Screen sizes
 
 This works well at 1080p full screen in Chrome with presentation mode,
---
+++ fabfile.py
@@ -0,0 +1,205 @@
+"""Manage a remotely-installed WSGI app on a Unix-like system, with
+environment variables controlling a lot of the WSGI app's
+functionality (12 Factor style).
+
+All remote access to the app is done via an `invoke` script, which
+contains the environment variables, which is created during setup.
+Sensitive ones should be passed through fab's env rather than being
+put directly into this script. (So this should remain valid for the
+lifetime of an open source project. Hostnames, db credentials and the
+like do not belong in here. We do have a default username and home
+directory, for convenience.)
+
+We create a virtual environment for every release. You probably want
+to delete them after a while (but you probably want to delete the
+releases as well). This is "better" than sharing a virtualenv, because
+of the way pip upgrades packages (otherwise you will get periods where
+the app will not work if it needs non-code files or just
+previously-unused packages). It is, however, slower.
+
+(Heroku's slug compilation is a better approach. It'd be nice to
+detect differences and re-use virtualenvs using symlinking or
+copy-and-upgrade in future. However we're not really here to build a
+cheap PaaS.)
+
+Getting started:
+
+* with a remote user & host you have access to
+$ fab -H HOST setup
+
+* to put the latest master live
+$ fab -H HOST deploy
+
+* if something goes wrong, roll back to a specific version
+$ fab -H switch_to:version=<VERS> restart_appserver
+
+deploy will apply migrations; switch_to will not. Also, migrations are
+applied while the site is still running, so should be backwards
+compatible.
+
+(deploy also runs compilestatic and compilemessages)
+
+Remote server layout (setup makes these):
+
+archives            tgz archives of code versions
+releases            unpacked, versioned by datetime of fabric invocation
+releases/current    symlink to current version
+releases/previous   symlink to previous version
+releases/next       symlink to version being upgraded to
+releases/<>/ENV     virtualenv per release
+userv/rc            userv script for starting app server
+invoke              environment-setting invocation script (acts both
+                    as an init.d script and a way of invoking app tasks
+                    such as migration, compilestatic
+"""
+
+from fabric.api import *
+from fabric.contrib.files import exists
+import tempfile
+import time
+
+import fabhelpers
+
+
+env.remote = 'git@github.com:jaylett/kallisto.git'
+env.branch = 'master'
+env.project = 'kallisto'
+
+env.user = 'kallisto'
+env.path = '/home/%s' % env.user
+
+
+def deploy():
+    """
+    Deploy the latest version of the site to the servers.
+    """
+
+    # installs any required third party modules, compiles static files
+    # and messages, migrates the database and then restarts the
+    # appserver
+    
+    env.release = time.strftime('%Y-%m-%dT%H.%M.%S')
+
+    fabhelpers.export_and_upload_tar_from_git()
+    prep_release(env.release)
+    switch_to(env.release)
+    restart_appserver()
+
+    
+def switch_to(version):
+    """Switch the current (ie live) version."""
+    
+    require('hosts')
+
+    previous_path = os.path.join(env.path, 'releases', 'previous')
+    current_path = os.path.join(env.path, 'releases', 'current')
+    if exists(previous_path):
+        run('rm %s' % previous_path)
+    if exists(current_path):
+        run('mv %s %s' % (current_path, previous_path))
+    # ln -s doesn't actually take a path relative to cwd as its first
+    # argument; it's actually relative to its second argument
+    run('ln -s %s %s' % (version, current_path))
+    # tidy up the next marker if there was one
+    run('rm -f %s' % os.path.join(env.path, 'releases', 'next'))
+    
+    env.release = version # in case anything else wants to use it after us
+
+    
+def prep_release(version):
+    """Compile static, make messages and migrate."""
+
+    require('hosts')
+    
+    next_path = os.path.join(env.path, 'releases', 'next')
+    if exists(next_path):
+        run('rm %s' % next_path)
+    run('ln -s %s %s' % (version, next_path))
+
+    run(
+        "cd %(path)s/releases/next; "
+        "virtualenv ENV; "
+        "ENV/bin/pip install -E ENV -r requirements.txt" % {
+            'path': env.path,
+            'release': env.release
+        }
+    )
+
+    run('invoke prep')
+    # leave the next marker (symlink) in place in case something
+    # goes wrong before the end of switch_to, since it will provide
+    # useful state on the remote machine
+
+
+def app_shell():
+    """Get an app shell on the current release."""
+
+    require('hosts')
+
+    run("invoke shell")
+
+
+def restart_appserver():
+    """Restart the (gunicorn) app server."""
+
+    require('hosts')
+    
+    run("invoke restart")
+
+    
+def setup():
+    """Set up the initial structure for the given user."""
+    
+    require('hosts', 'path')
+    require(
+        'database_url',
+        'django_secret_key',
+        'allowed_hosts',
+        'port',
+        used_for="configuring the application.",
+    )
+
+    # make our directory structure
+    run("mkdir $s/releases" % env.path)
+    run("mkdir $s/archives" % env.path)
+    # make the userv rc script
+    run("mkdir %s/.userv" % env.path)
+    put("userv.rc.in", "%s/.userv/rc" % env.path)
+    # and the script it points to
+    # @TOPDIR@ -> env.path
+    # @WSGI@ -> $(env.project).wsgi (python path to WSGI app)
+    # @DATABASE_URL@ -> syntax postgresql://USER:PASSWORD@localhost:5432/DBNAME
+    # (or postgis://...)
+    # @DJANGO_SECRET_KEY@ -> what it says (make it long and gnarly)
+    # @ALLOWED_HOSTS@ -> semicolon separated (eg loose-end.in;www.loose-end.in)
+    # @PORT@ -> that gunicorn should listen on
+    #
+    # The last four should be passed into the env in a fab-ish manner.
+    # (Hence the require statements above.)
+
+    substitutions = (
+        ('TOPDIR', env.path),
+        ('WSGI', '%s.wsgi' % env.project),
+        ('DATABASE_URL', env.database_url),
+        ('DJANGO_SECRET_KEY', env.django_secret_key),
+        ('ALLOWED_HOSTS', env.allowed_hosts),
+        ('PORT', env.port),
+    )
+
+    tmpfile = tempfile.NamedTemporaryFile(delete=False)
+    commands = '; '.join(
+        "s/@%(var)s@/%(subst)s/g; " % {
+            'var': k,
+            'subst': v.replace("'", r"\'").replace("/", r"\/"),
+        } for k, v in substititions
+    )
+    try:
+        local(
+            "sed < invoke.in > $(tmp_fname)s '%(commands)'" % {
+                'tmp_fname': tmpfile.name,
+                'commands': commands,
+            }
+        )
+        put(tmpfile.name, "%(path)s/invoke" % env.path)
+    finally:
+        os.remove(tmpfile.name)
